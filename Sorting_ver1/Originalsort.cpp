ee7ffa57eddaa80cae97c7a2ea1457af908de8ee
秀隆 真鍋 <wotto27oct@gmail.com>
2017-11-06 09:10:30 +0900

    Fix Original sort

--- Sorting_ver1/Originalsort.cpp
+++ Sorting_ver1/Originalsort.cpp
@@ -1,20 +1,32 @@
 #include "Originalsort.h"
 
-void original_sort(vector<int> A,int n) {
-	vector<int> B(n+1, 0);
-	vector<int> C(n, 0);
+void original_sort(vector<int> A) {
+	int n = 0;
+	for (int i = 0; i < A.size(); ++i) {
+		if (n < A[i]) n = A[i];
+	}
+
+	vector<int> B(n, 0);
+	vector<int> C(A.size() , 0);
 	for (int i = 0; i < A.size();i++) {
-		++B[A[i]];
+		++B[A[i] - 1];
 	}
 	int j = 0;
-	for (int i = 1; i < B.size(); i++) {
+
+	for (int i = 0; i < B.size(); i++) {
 		while (B[i] != 0) {
-			C[j] = i;
+			C[j] = i + 1;
 			--B[i];
+			if (j + 1 >= n)
+			{
+				break;
+			}
 			++j;
 		}
 	}
-	/*for (int i = 0; i < C.size(); i++) {
+
+	//testing
+	for (int i = 0; i < C.size(); i++) {
 		cout << C[i] << ",";
-	}*/
+	}
 }
